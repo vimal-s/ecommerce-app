52151a4b4bad144562d69b1b9c04cfd4aeaf7eb6
vimal <36556890+vimal-s@users.noreply.github.com>
2020-10-01 20:16:52 +0530

    Rename main class

--- src/main/java/com/example/demo/SpringSecurityApplication.java
+++ src/main/java/com/example/demo/SpringSecurityApplication.java
@@ -10,12 +10,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication
-public class SareetaApplication {
+public class SpringSecurityApplication {
 
 	private String str;
 
 	public static void main(String[] args) {
-		SpringApplication.run(SareetaApplication.class, args);
+		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
 	@Bean
--- src/test/java/com/example/demo/SpringSecurityApplicationTests.java
+++ src/test/java/com/example/demo/SpringSecurityApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SareetaApplicationTests {
+public class SpringSecurityApplicationTests {
 
 	@Test
 	public void contextLoads() {
